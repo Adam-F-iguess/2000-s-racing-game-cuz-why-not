0f8757a97601db732f1215d4bab832f58eb59d54
Adam-F-iguess <adam.faseeh@student.education.wa.edu.au>
2025-02-14 17:38:58 +0800

    Added a 3d render of cube (from https://github.com/LeetUnova/Pygame-3D-Graphics?source=post_page-----c36ec2e03a33---------------------------------------)
    modified code to move pov/cube a bit

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import pygame
 import sys
 import math
 import pygame_init
+
+screen, font = pygame_init.init()
+
--- pygame_init.py
+++ pygame_init.py
@@ -1,1 +1,10 @@
 import pygame
+def init():
+    pygame.mixer.init()
+    pygame.font.init()
+    font = pygame.font.Font('Fonts/NotoSansJP-VariableFont_wght.ttf', 32)
+    SCREEN_WIDTH = 1200
+    SCREEN_HEIGHT = 700
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Gameshow')
+    return screen, font
